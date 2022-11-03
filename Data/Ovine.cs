6d8fd69ed2beafae07e940741220b46c54cb7892
Benjamin Setterfield <bpsetterfield@gmail.com>
2022-11-03 14:04:06 +0000

    Update Ovine.cs
    Co-Authored-By: Tom <19192363@studentmail.ul.ie>
    Co-Authored-By: Jim Ryan <jimmih15@gmail.com>

--- Data/Ovine.cs
+++ Data/Ovine.cs
@@ -29,16 +29,15 @@ public abstract class Ovine : Animal
     private readonly OvineBreed _breed;
     private double _woolYield;
 
-    public Ovine(int id, DateTime dob, Sex sex, OvineBreed _breed) : base(id, dob, sex)
+    public Ovine(int id, DateTime dob, Sex sex, OvineBreed _breed, double woolYield) : base(id, dob, sex)
     {
         this._breed = _breed;
+        _woolYield = woolYield;
     }
 
 
-    public double estimatedWoolYield()
+    public double EstimatedWoolYield()
     {
-        //need business logic here
-
         switch (_breed)
         {
             case OvineBreed.BROWN_SWISS:
@@ -47,6 +46,44 @@ public abstract class Ovine : Animal
                 return 2.1;
             case OvineBreed.CORRIE:
                 return 2.0;
+            case OvineBreed.EAST_FRIESIAN:
+                return 1.9;
+            case OvineBreed.FINNISH_LANDRACE:
+                return 1.8;
+            case OvineBreed.HAMPSHIRE:
+                return 1.7;
+            case OvineBreed.HORNED_DORSET:
+                return 1.6;
+            case OvineBreed.ILE_DE_FRANCE:
+                return 1.5;
+            case OvineBreed.KERRY_HILL:
+                return 1.4;
+            case OvineBreed.LINCOLN:
+                return 1.3;
+            case OvineBreed.MANX_LOAGHTAN:
+                return 1.2;
+            case OvineBreed.MONTBELIARDE:
+                return 1.1;
+            case OvineBreed.NORFOLK_HORN:
+                return 1.0;
+            case OvineBreed.NORWEGIAN_DALE:
+                return 3.1;
+            case OvineBreed.POLLED_DORSET:
+                return 3.0;
+            case OvineBreed.SHROPSHIRE:
+                return 3.1;
+            case OvineBreed.SOUTH_DOWN:
+                return 3.5;
+            case OvineBreed.TEXEL:
+                return 2.2;
+            case OvineBreed.WELSH_MOUNTAIN:
+                return 5.3;
+            case OvineBreed.WELSH_WHITE:
+                return 4.2;
+            case OvineBreed.WEST_FRIESIAN:
+                return 3.8;
+            case OvineBreed.YAKIMA:
+                return 2.6;
             default:
                 return 0;
         }
