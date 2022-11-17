c939566263b1d52a475df87e6e71af9183736821
Benjamin Setterfield <bpsetterfield@gmail.com>
2022-11-17 14:43:20 +0000

    ORM

---
+++ ORM/database.cs
@@ -0,0 +1,48 @@
+﻿// Database.cs
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using CS4125.Data.AnimalData;
+using CS4125.Data.UserData;
+
+namespace CS4125.ORM
+{
+    
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Site> SiteData { get; set; }
+
+        public DbSet<Animal> AnimalData { get; set; }
+
+        public DbSet<Farmer> FarmerData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
+    }
+
+    public class SiteData
+    {
+        public siteID { get; set; }
+        public siteAddress { get; set; }
+    }
+    
+    public class AnimalData
+    {
+        public animalID { get; set; }
+        public animalType { get; set; }
+        public animalDOB { get; set; }
+        public animalBreed { get; set; }
+        public animalGender { get; set; }
+        public animalSiteID { get; set; }
+    }
+    
+    public class FarmerData
+    {
+        public farmerID { get; set; }
+        public farmerName { get; set; }
+        public farmerEmail { get; set; }
+    }
+}
---
+++ ORM/designer.cs
@@ -0,0 +1,18 @@
+﻿// ORM implementation using EFcore
+
+public class ApplicationDbContext : DbContext
+{
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>().ToTable("Blog");
+        modelBuilder.Entity<Post>().ToTable("Post");
+    }
+}
