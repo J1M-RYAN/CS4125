1ee03b6c9349f441555059d38b49dc1263bfda24
Jim Ryan <jimmih15@gmail.com>
2022-11-30 00:57:07 +0000

    no https redirection

--- Program.cs
+++ Program.cs
@@ -34,7 +34,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
